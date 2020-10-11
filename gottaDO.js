dafe033edef77cdb7a925c6e3e62fdf396d1fd4a
unknown <shreyakumar.71@gmail.com>
2020-10-11 18:29:40 +0530

    deleted useless code

--- gottaDO.js
+++ gottaDO.js
@@ -1,22 +1,2 @@
-function getToDoListData() {
-    let req = new XMLHttpRequest();
-    req.onreadystatechange = () => {
-        if (req.readyState == XMLHttpRequest.DONE) {
-          console.log(req.responseText);
-        }
-      };
-      
-      req.open("GET", "https://api.jsonbin.io/b/<BIN_ID>", true);
-      req.setRequestHeader("secret-key", "<SECRET_KEY>");
-      req.send();
-      console.log(req.responseText);
-      var responseText = JSON.parse(req.responseText);
-  }
-
-  function setDataInBox(responseText){
-    document.getElementById("statusMessage").innerHTML = response1.statusMessage;
-
-  }
-
 
 
